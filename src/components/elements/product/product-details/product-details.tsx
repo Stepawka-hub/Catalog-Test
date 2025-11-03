8d72f04d4eaab865058edcc94b0d728cc48bfd5f
Stepawka <stepan-lvov-04@mail.ru>
2025-11-03 05:49:21 +0300

    styles: responsively style a product page

--- src/components/elements/product/product-details/product-details.tsx
+++ src/components/elements/product/product-details/product-details.tsx
@@ -28,12 +28,14 @@ export const ProductDetailsUI: FC<TProductDetailsProps> = ({
   };
 
   return (
-    <div className="grid grid-cols-2 gap-8 max-w-[60vw] mx-auto">
+    <div className="grid gap-4 md:grid-cols-1 lg:grid-cols-2 xl:gap-4 xl:max-w-[80vw] 2xl:max-w-[70vw] mx-auto">
       <Gallery name={title} thumbnail={thumbnail} images={images} />
 
-      <div className="flex flex-col gap-2 p-8 bg-neutral-900 rounded-lg">
+      <div className="flex flex-col gap-2 p-4 sm:p-6 md:p-8 bg-neutral-900 rounded-lg">
         <div className="flex justify-between items-center mt-4 gap-2">
-          <h1 className="text-3xl font-medium">{title}</h1>
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-medium">
+            {title}
+          </h1>
           <LikeButton
             isLiked={isLiked}
             className="text-2xl"
@@ -49,9 +51,13 @@ export const ProductDetailsUI: FC<TProductDetailsProps> = ({
             { label: "Артикул", value: sku },
           ]}
         />
-        <div className="flex justify-between items-end mt-auto font-medium">
-          <span className="text-xl">В наличии: {stock} шт.</span>
-          <span className="text-2xl text-green-500">{price} ₽</span>
+        <div className="flex justify-between items-end gap-2 mt-5 lg:mt-auto font-medium">
+          <span className="text-base sm:text-lg md:text-xl">
+            В наличии: {stock} шт.
+          </span>
+          <span className="text-lg sm:text-xl md:text-2xl text-green-500">
+            {price} ₽
+          </span>
         </div>
       </div>
     </div>
--- src/components/elements/shared/details-list/details-list.tsx
+++ src/components/elements/shared/details-list/details-list.tsx
@@ -4,7 +4,7 @@ import { TDetailsListProps } from "./types";
 export const DetailsList: FC<TDetailsListProps> = ({ items }) => (
   <div className="flex flex-col gap-3">
     {items.map(({ label, value }, idx) => (
-      <span key={idx}>
+      <span key={idx} className="text-[0.9rem] sm:text-base">
         <span className="text-neutral-400">{label}:</span> {value || "нет"}
       </span>
     ))}
